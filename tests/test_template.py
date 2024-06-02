c148e5d2759d200772e7f7b7cd2f99e2d2feef62
giocaizzi <giocaizzi@gmail.com>
2024-06-02 18:29:41 +0200

    before conditionals

--- tests/test_template.py
+++ tests/test_template.py
@@ -1,6 +1,7 @@
 """test template"""
 
 import pytest
+from pytest_cookies.plugin import Result
 
 # Bake Result
 # cookies.bake() returns a result instance with a bunch of fields that hold useful information:
@@ -10,29 +11,74 @@ import pytest
 # project_path: a Path object pointing to the rendered project
 # context: is the rendered context
 
+RECIPE = {
+    "package_name": "pyprova",
+    "short_description": "This is a prova.",
+}
+
+RECIPES = (
+    None,
+    RECIPE,
+    {**RECIPE, "setup_workflows": "false"},
+)
+
 
 @pytest.fixture
 def bake(cookies):
-    result = cookies.bake(
-        extra_context={
-            "package_name": "pyprova",
-            "short_description": "This is a prova.",
-        }
-    )
-    return result
+    def _bake(extra_context=None):
+        if extra_context is None:
+            # Use default parameters
+            extra_context = RECIPE
+        cookie = cookies.bake(extra_context=extra_context)
+        return cookie
+
+    return _bake
 
 
-def test_build_success(bake):
-    result = bake
+# ---- Test success of recepies ----
+@pytest.mark.parametrize(
+    "recipe",
+    RECIPES,
+)
+def test_build_success(bake, recipe):
+    """test that the recipes build successfully"""
+    result = bake(recipe)
 
     # build success
     assert result.exit_code == 0
     assert result.exception is None
 
 
-def test_folder_structure(bake):
-    result = bake
+# ---- Test project structure ----
+@pytest.mark.parametrize("recipe", RECIPES)
+def test_folder_structure(bake, recipe):
+    """test folder stru"""
+    result = bake(recipe)
 
     # folder structuree
     assert result.project_path.name == "pyprova"
     assert result.project_path.is_dir()
+
+    # license
+    assert result.project_path.joinpath("LICENSE.md").is_file()
+    # readme
+    assert result.project_path.joinpath("README.md").is_file()
+    # gitignore
+    assert result.project_path.joinpath(".gitignore").is_file()
+    # # workflows
+    # assert result.project_path.joinpath(".github.is_dir()")
+    # assert result.project_path.joinpath(".github/workflows").is_dir()
+
+
+# ---- Test workflow choice ----
+
+
+@pytest.mark.parametrize(
+    "recipe",
+    RECIPES,
+)
+def test_workflow_choice(bake, recipe):
+    result = bake(recipe)
+
+    # workflows
+    assert result.project_path.joinpath(".github/workflows").is_dir()
