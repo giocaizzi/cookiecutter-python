bb7f3960b93b21eb11ac353ac2b565be856b9e8e
Giorgio Caizzi <64302135+giocaizzi@users.noreply.github.com>
2024-06-15 15:44:52 +0200

    colors and fix package_slug #11 (#19)

--- {{cookiecutter.folder_name}}/{{cookiecutter.package_slug}}/__init__.py
+++ {{cookiecutter.folder_name}}/{{cookiecutter.package_slug}}/__init__.py
@@ -1,12 +1,12 @@
-"""{{cookiecutter.package_name}}"""
-
+"""{{cookiecutter.package_slug}}"""
+{% if cookiecutter.package_specs == "setup.py" %}
 __author__ = "{{cookiecutter.author_name}}"
 __email__ = "{{cookiecutter.author_email}}"
 __license__ = "{{cookiecutter.license}}"
 __copyright__ = "Copyright (c) {% now 'utc', '%Y' %} {{cookiecutter.author_name}}"
 __url__ = (
-    "https://github.com/{{cookiecutter.github_username}}/{{cookiecutter.package_name}}"
+    "https://github.com/{{cookiecutter.github_username}}/{{cookiecutter.package_slug}}"
 )
 __version__ = "{{cookiecutter.version}}"
-
+{% endif %}
 __all__ = []
